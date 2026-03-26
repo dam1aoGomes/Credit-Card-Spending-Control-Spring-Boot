d626ebde161745806323c08f3b19f92d13172635
Damião Neto <damiao28.contato@gmail.com>
2026-03-26 10:37:30 -0300

    chore: setup inicial do projeto com spring data jpa e postgres

---
+++ src/test/java/com/damiao/controledegastos/ControledegastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiao.controledegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledegastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
